b54ea08485b55ec9c384e6c529af12dcc83426ca
jlnavas3 <jlnavas3@utpl.edu.ec>
2026-09-24 19:38:28 -0500

    Update Kotlin Compose and Serialization plugins to 2.2.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
     id("com.android.application") version "9.4.1" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21" apply false
 }
